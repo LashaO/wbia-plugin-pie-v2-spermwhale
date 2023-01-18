e84f0862b1c3c69baa5e117bfdb83e3e09e380b0
LashaO <otarashvililasha@gmail.com>
2023-01-18 15:46:53 +0400

    a

--- wbia_pie_v2/engine/wandb_utils.py
+++ wbia_pie_v2/engine/wandb_utils.py
@@ -8,7 +8,7 @@ def init_wandb(exp_name=None, config={}):
 
     os.environ["WANDB_MODE"] = "online"
     os.environ["WANDB_API_KEY"] = "a7b8fd4b9c4346e595e83c9bec00b807f133d853"
-    wandb.init(project="PIE-spermwhale", name=exp_name)
+    wandb.init(project="PIE-bottlenose_dolphin", name=exp_name)
     wandb.config = config  # {"learning_rate": 0.001, "epochs": 100, "batch_size": 128}
 
     return wandb
